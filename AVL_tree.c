f26cf1d5a3e3513992468bfa9bb382f4acb20d55
Adrianne Alves <driah_as@hotmail.com>
2018-04-19 18:17:08 -0300

    Improving structure

--- AVL_tree.c
+++ AVL_tree.c
@@ -9,10 +9,8 @@ typedef struct no {
   struct no *esquerda, *direita;
 } No;
 
-/*
- * Calcula a altura da arvore ou de uma subarvore
- * A funcao eh chamada recursivamente aa esquerda e aa direita (para identificar de qual lado a altura eh maior)
- */
+ // Calcula a altura da arvore ou de uma subarvore
+
 int altura(No* t) {
     if (t == NULL) {
         return 0;
@@ -22,12 +20,8 @@ int altura(No* t) {
     return hEsquerda > hDireita ? hEsquerda + 1 : hDireita + 1; // Retorna a maior altura calculada mais o pr�prio n�
 }
 
-/*
- * Cria um 'no' na arvore
- * o primeiro parametro eh o valor do 'no' que sera criado
- * o segundo eh o numero da linha no arquivo
- * os demais sao os 'nos' da esquerda e da direita
-*/
+// Cria um 'no' na arvore
+
 No* cria(int valor, No* esquerda, No* direita) {
     No* n = (No*) malloc (sizeof(No));
     n->valor = valor;
@@ -37,9 +31,8 @@ No* cria(int valor, No* esquerda, No* direita) {
     return n;
 }
 
-/*
- * Bonus - verifica se a arvore (ou 'no') esta balanceada(o)
- */
+ // Verifica se a arvore (ou 'no') esta balanceada(o)
+
 int verifica_AVL(No* t) {
     if (t == NULL)
         return 1;
@@ -47,8 +40,6 @@ int verifica_AVL(No* t) {
 }
 
 void listagem(No *t) {
-
-  // Verifica se a arvore esta vazia
    if(t != NULL){
      listagem(t->esquerda);
      printf("\n%d", t->valor);
@@ -56,26 +47,23 @@ void listagem(No *t) {
    }
 }
 
+// Verifica se a arvore esta balanceada (eh AVL)
+// Chama a funcao escreve para imprimir elementos de forma ordenada
 
-/*
- * verifica_escreve primeiro verifica se a arvore esta balanceada (eh AVL)
- * e depois chama a funcao escreve que imprime os elementos de forma ordenada
- */
-void verifica_escreve(No* t) {
-    if (t == NULL) {
+void verifica_escreve(No* temp) {
+    if (temp == NULL) {
       printf("\nArvore vazia!\n");
-    }else if (!verifica_AVL(t)) {
+    }else if (!verifica_AVL(temp)) {
       printf("\nArvore nao esta balanceada(AVL)!\n");
     }else{
-      listagem(t);
+      listagem(temp);
+      printf("\n");
     }
-    printf("\n\nPressione a tecla Enter para retornar.\n");
 }
 
-/*
- * rotacao do 'no' a esquerda apos uma inclusao a esquerda
- *
- */
+
+ // rotacao do 'no' a esquerda apos uma inclusao a esquerda
+
 void esquerdaEsquerda(No** r) {
   No* b = *r;
   No* a = b->esquerda;
@@ -86,10 +74,6 @@ void esquerdaEsquerda(No** r) {
   *r = a;
 }
 
-/*
- *
- *
- */
 void direitaDireita(No** r) {
   No* a = *r;
   No* b = a->direita;
@@ -100,10 +84,8 @@ void direitaDireita(No** r) {
   *r = b;
 }
 
-/*
- * rotacao do 'no' a direita apos uma inclusao a esquerda
- *
- */
+// rotacao do 'no' a direita apos uma inclusao a esquerda
+
 void esquerdaDireita(No** r) {
   No *c = *r;
   No *a = c->esquerda;
@@ -130,10 +112,8 @@ void esquerdaDireita(No** r) {
   *r = b;
 }
 
-/*
- * rotacao do 'no' a direita apos uma inclusao a esquerda
- *
- */
+// rotacao do 'no' a direita apos uma inclusao a esquerda
+
 void direitaEsquerda(No** r) {
   No *a = *r;
   No *c = a->direita;
@@ -160,15 +140,10 @@ void direitaEsquerda(No** r) {
   *r = b;
 }
 
+// Função que procura local em que novo nó deve ser incluído
 
-/*
- * aux_insere ira incluir valor no 'no' "t", quando este for nulo
- * enquanto o 'no' n�o for nulo, a funcao ira comparar o valor de "t" com o valor a ser inserido
- * e chamar a si propria recursivamente ate encontrar o ponto da arvore onde "valor" deve ser incluido
- *
- *   *cresceu indica se a insercao teve sucesso
- */
 int aux_insere(No** t, int valor, int *cresceu) {
+
     // Se o 'no' atual for nulo, inclui os valores neste 'no'
     if (*t == NULL) {
         *t = cria(valor, NULL, NULL);
@@ -214,7 +189,7 @@ int aux_insere(No** t, int valor, int *cresceu) {
     // Se chegou aqui eh porque o valor a ser inserido eh MAIOR que o valor do 'no' atual,
     // logo a chamada recursiva ocorrera a DIREITA
 
-    // Se a inclusao aa direita tiver sucesso, faz o balanceamento
+    // Se a inclusao a direita tiver sucesso, faz o balanceamento
     if (aux_insere(&(*t)->direita, valor, cresceu)) {
         if (*cresceu) {
             switch ((*t)->balanceamento) {
@@ -243,11 +218,9 @@ int aux_insere(No** t, int valor, int *cresceu) {
 
 }
 
-/*
- * insere um 'no' na arvore
- * chama aux_insere que eh a funcao que identificara o 'no' da arvore onde o valor deve ser incluido
- * se o valor ja existir, nao sera incluido novamente e o retorno sera 0. Se for incluido, o retorno sera 1
- */
+// insere um 'no' na arvore
+// Procura local onde nó será incluído
+
 int insere(No **t) {
   int cresceu, valor;
   printf("Digite o valor que deseja inserir: " );
@@ -255,35 +228,6 @@ int insere(No **t) {
   return aux_insere(t, valor, &cresceu);
 }
 
-
-/*
- *  localizaValor eh um metodo recursivo que percorre a arvore cujo 'no' raiz foi recebido no primeiro parametro
- *  em busca do valor informado no segundo parametro
- */
-// No* localizaValor(No *t, int chavePesquisa) {
-//     // Se nao encontrou, retorna NULL
-//     if (t == NULL) {
-//         return t;
-//     }
-//     // Se encontrou retorna o 'no' que contem o valor
-//     if (valor == (*t)->valor) {
-//         return t;
-//     }
-//
-//     if (valor < (*t)->valor) {
-//         // Se o valor eh MENOR que o valor do 'no' atual, a chamada recursiva ocorrera a ESQUERDA
-//         return localizaValor(t->esquerda, chavePesquisa);
-//     } else {
-//         // Se o valor eh MAIOR que o valor do 'no' atual, a chamada recursiva ocorrera a DIREITA
-//         return localizaValor(t->direita, chavePesquisa);
-//     }
-//
-// }
-
-
-/*
-*  Exibe as opcoes ao usuario
-*/
 char menuPrincipal() {
     int opcao;
     printf("|--------------------------|\n");
@@ -292,16 +236,13 @@ char menuPrincipal() {
     printf("\nEscolha sua opcao\n");
     printf("1 - Inserir valor na arvore\n");
     printf("2 - Listar arvore\n");
+    printf("3 - Deletar Valor\n");
     printf("0 - Sair\n");
     printf("\nInforme o numero da operacao desejada: ");
     scanf("%d", &opcao);
     return opcao;
 }
 
-/*
- * main controla a execucao do programa
- *
- */
 int main() {
     int opcao;
     No *t = NULL;
@@ -314,6 +255,9 @@ int main() {
                 break;
             case 2:
                 verifica_escreve(t);
+                break;
+            case 3:
+                printf("Deleta\n");
                 break;
             case 0:
                 exit(0);
